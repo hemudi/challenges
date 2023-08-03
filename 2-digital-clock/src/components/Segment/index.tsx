a13f6aa025f11fe4c7e83cc315e62055b1c5b5e4
Hemudi <ksum1205@naver.com>
2023-08-03 17:59:56 +0900

    :sparkles: Add Segment component

---
+++ 2-digital-clock/src/components/Segment/index.tsx
@@ -0,0 +1,31 @@
+export type SegmentStatus = 'on' | 'off';
+
+interface SegmentProps {
+  status?: SegmentStatus;
+  align?: 'vertical' | 'horizontal';
+  className?: string;
+}
+
+const colorStyle = {
+  on: 'bg-cyan-500',
+  off: 'bg-slate-300',
+};
+
+const alignStyle = {
+  vertical: 'w-14 h-3',
+  horizontal: 'w-3 h-14',
+};
+
+const Segment = ({
+  status = 'on',
+  align = 'vertical',
+  className = '',
+}: SegmentProps) => {
+  return (
+    <div
+      className={`${alignStyle[align]} ${colorStyle[status]} ${className} rounded`}
+    />
+  );
+};
+
+export default Segment;
