3625163b39a6b8b6690b3aec0f175c39ab243456
Hemudi <ksum1205@naver.com>
2023-08-04 00:51:45 +0900

    :sparkles: Add TimePanel Separator component

---
+++ 2-digital-clock/src/components/TimePanel/Separator/index.stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Separator from '.';
+
+const meta: Meta<typeof Separator> = {
+  title: 'Components/Separator',
+  component: Separator,
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Separator>;
+export const Default: Story = {};
---
+++ 2-digital-clock/src/components/TimePanel/Separator/index.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import useInterval from '../../../hooks/useInterval';
+
+const colorStyle = {
+  on: 'bg-cyan-500',
+  off: 'bg-slate-300',
+};
+
+const Separator = () => {
+  const [isOn, setIsOn] = useState<boolean>(false);
+
+  useInterval(() => setIsOn((prev) => !prev), 1000, false);
+
+  return (
+    <div className="flex h-full w-3 flex-col justify-around">
+      <div
+        className={`h-3 w-3 rounded ${isOn ? colorStyle.on : colorStyle.off}`}
+      />
+      <div
+        className={`h-3 w-3 rounded ${isOn ? colorStyle.on : colorStyle.off}`}
+      />
+    </div>
+  );
+};
+
+export default Separator;
