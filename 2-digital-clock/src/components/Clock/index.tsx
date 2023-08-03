e821733e2e8622f828d54a6dda128521efce23c9
Hemudi <ksum1205@naver.com>
2023-08-04 02:53:00 +0900

    :sparkles: Implement Clock Mode

--- 2-digital-clock/src/components/Clock/index.tsx
+++ 2-digital-clock/src/components/Clock/index.tsx
@@ -1,15 +1,42 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
 import TimePanel from '../TimePanel';
 import ToggleButton from '../ToggleButton';
 import Button from '../Button';
+import { HourFormat, Time, getCurrentTime } from '../../utils/time';
+import useInterval from '../../hooks/useInterval';
 
 type ClockMode = 'CLOCK' | 'TIMER';
 
 const CLOCK_MODE = 'CLOCK';
 const TIMER_MODE = 'TIMER';
 
+const HOUR_FORMAT_12 = 12;
+const HOUR_FORMAT_24 = 24;
+
+const initTime: Time = {
+  hours: 0,
+  minutes: 0,
+  period: null,
+};
+
 const Clock = () => {
+  const [isStart, setIsStart] = useState<boolean>(false);
   const [mode, setMode] = useState<ClockMode>(CLOCK_MODE);
+  const [hourFormat, setHourFormat] = useState<HourFormat>(HOUR_FORMAT_12);
+  const [{ hours, minutes, period }, setTime] = useState<Time>(initTime);
+
+  useEffect(() => {
+    setTime(mode === CLOCK_MODE ? getCurrentTime(hourFormat) : initTime);
+    setIsStart(true);
+  }, [mode, hourFormat]);
+
+  useInterval(
+    () => {
+      setTime(mode === CLOCK_MODE ? getCurrentTime(hourFormat) : initTime);
+    },
+    1000,
+    !isStart,
+  );
 
   const modeToggleHandler = ({
     currentTarget,
@@ -19,7 +46,7 @@ const Clock = () => {
 
   return (
     <div className="flex w-fit flex-col items-center justify-center rounded-xl border-2 bg-cyan-50 p-6">
-      <TimePanel hours={24} minutes={24} description={['PM']} />
+      <TimePanel hours={hours} minutes={minutes} period={period} />
       <div className="flex w-full justify-between p-2">
         <div className="flex w-fit flex-col">
           <div className="flex w-full justify-between">
@@ -29,8 +56,12 @@ const Clock = () => {
           <ToggleButton onChange={modeToggleHandler} />
         </div>
         <div className="flex w-fit gap-2 rounded text-xl">
-          <Button>12</Button>
-          <Button>24</Button>
+          <Button onClick={() => setHourFormat(HOUR_FORMAT_12)}>
+            {HOUR_FORMAT_12}
+          </Button>
+          <Button onClick={() => setHourFormat(HOUR_FORMAT_24)}>
+            {HOUR_FORMAT_24}
+          </Button>
         </div>
       </div>
     </div>
