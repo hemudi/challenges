8c8ed82c00644c7466102a592506388c2abe1c66
Hemudi <ksum1205@naver.com>
2023-08-03 18:30:48 +0900

    :sparkles: Add Button component

---
+++ 2-digital-clock/src/components/Button/index.tsx
@@ -0,0 +1,20 @@
+import { MouseEventHandler, ReactNode } from 'react';
+
+interface ButtonProps {
+  className?: string;
+  children: ReactNode;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+const Button = ({ children, ...props }: ButtonProps) => {
+  return (
+    <button
+      className="h-full w-full rounded p-3 text-white enabled:bg-cyan-500 enabled:hover:bg-cyan-600 enabled:active:bg-cyan-500"
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
