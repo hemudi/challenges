a7310e40a01908636ed481f9c74aa859e3c7f543
Hemudi <ksum1205@naver.com>
2023-08-03 16:22:33 +0900

    :heavy_plus_sign: Install project packages

---
+++ 2-digital-clock/src/App.tsx
@@ -0,0 +1,11 @@
+import Segment from './components/Segment';
+
+function App() {
+  return (
+    <div className="App">
+      <Segment value={1} align="vertical" />
+    </div>
+  );
+}
+
+export default App;
