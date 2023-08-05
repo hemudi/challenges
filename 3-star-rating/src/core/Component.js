38152b880b793b41bc6bfa3246b21d7449755a9b
Hemudi <ksum1205@naver.com>
2023-08-06 04:45:02 +0900

    :sparkles: Core Component class 구현

---
+++ 3-star-rating/src/core/Component.js
@@ -0,0 +1,37 @@
+export default class Component {
+  $target;
+  $props;
+  $state;
+
+  constructor($target, $props) {
+    this.$target = $target;
+    this.$props = $props;
+    this.init();
+  }
+
+  init() {
+    this.setup();
+    this.render();
+    this.setEvent();
+  }
+
+  setup() {}
+
+  template() {
+    return "";
+  }
+
+  mounted() {}
+
+  render() {
+    this.$target.innerHTML = this.template();
+    this.mounted();
+  }
+
+  setState(newState) {
+    this.$state = { ...this.$state, ...newState };
+    this.render();
+  }
+
+  setEvent() {}
+}
