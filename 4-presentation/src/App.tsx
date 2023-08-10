8b29d1a25ef8759bac4699292a8820c5b557f289
Hemudi <ksum1205@naver.com>
2023-08-10 19:46:16 +0900

    :tada: Vite + React 프로젝트 초기 설정

---
+++ 4-presentation/src/App.tsx
@@ -0,0 +1,11 @@
+import Button from "@components/Button";
+
+function App() {
+  return (
+    <div className="flex w-screen h-screen justify-center items-center">
+      <Button>버튼</Button>
+    </div>
+  );
+}
+
+export default App;
